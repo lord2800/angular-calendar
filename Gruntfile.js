abb3deb83f9591424c036acdddcc537e00cc3076
Jeff Hubbard <lord2800@gmail.com>
2013-12-15 21:52:13 -0800

    Re-set silent mode, switch out burned token

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,7 +51,7 @@ module.exports = function (grunt) {
 				base: 'dist',
 				message: 'Auto-commit via grunt [ci-skip]',
 				repo: 'https://' + process.env.GH_OAUTH_TOKEN + '@github.com/lord2800/angular-calendar.git',
-				silent: false
+				silent: true
 			},
 			src: ['**']
 		}
