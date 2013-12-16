bc43d6dfa32d9e8c461a29c505f961d7c20d805c
Jeff Hubbard <lord2800@gmail.com>
2013-12-15 21:05:23 -0800

    Add test to ensure week/date list is accurate

--- test/directive.js
+++ test/directive.js
@@ -26,4 +26,49 @@ describe('Calendar Directive', function () {
 		local.$digest();
 		expect(isolate.year).toBe((new Date()).getFullYear());
 	});
+
+	it('should get the right set of week values for 2013-12-01', function () {
+		local.date = new Date(2013, 11);
+		local.$digest();
+
+		expect(isolate.weeks[0].days[0]).toBe(1);
+		expect(isolate.weeks[0].days[1]).toBe(2);
+		expect(isolate.weeks[0].days[2]).toBe(3);
+		expect(isolate.weeks[0].days[3]).toBe(4);
+		expect(isolate.weeks[0].days[4]).toBe(5);
+		expect(isolate.weeks[0].days[5]).toBe(6);
+		expect(isolate.weeks[0].days[6]).toBe(7);
+
+		expect(isolate.weeks[1].days[0]).toBe(8);
+		expect(isolate.weeks[1].days[1]).toBe(9);
+		expect(isolate.weeks[1].days[2]).toBe(10);
+		expect(isolate.weeks[1].days[3]).toBe(11);
+		expect(isolate.weeks[1].days[4]).toBe(12);
+		expect(isolate.weeks[1].days[5]).toBe(13);
+		expect(isolate.weeks[1].days[6]).toBe(14);
+
+		expect(isolate.weeks[2].days[0]).toBe(15);
+		expect(isolate.weeks[2].days[1]).toBe(16);
+		expect(isolate.weeks[2].days[2]).toBe(17);
+		expect(isolate.weeks[2].days[3]).toBe(18);
+		expect(isolate.weeks[2].days[4]).toBe(19);
+		expect(isolate.weeks[2].days[5]).toBe(20);
+		expect(isolate.weeks[2].days[6]).toBe(21);
+
+		expect(isolate.weeks[3].days[0]).toBe(22);
+		expect(isolate.weeks[3].days[1]).toBe(23);
+		expect(isolate.weeks[3].days[2]).toBe(24);
+		expect(isolate.weeks[3].days[3]).toBe(25);
+		expect(isolate.weeks[3].days[4]).toBe(26);
+		expect(isolate.weeks[3].days[5]).toBe(27);
+		expect(isolate.weeks[3].days[6]).toBe(28);
+
+		expect(isolate.weeks[4].days[0]).toBe(29);
+		expect(isolate.weeks[4].days[1]).toBe(30);
+		expect(isolate.weeks[4].days[2]).toBe(31);
+		expect(isolate.weeks[4].days[3]).toBeUndefined();
+		expect(isolate.weeks[4].days[4]).toBeUndefined();
+		expect(isolate.weeks[4].days[5]).toBeUndefined();
+		expect(isolate.weeks[4].days[6]).toBeUndefined();
+	});
 });
